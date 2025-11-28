37cd1f9a48b2a1aec225264f48687b63ab9dbc62
shiiimy <3593556160@qq.com>
2025-11-28 12:24:12 +0800

    feat:实现按钮高亮与任务过滤功能

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import Todolist from './components/todolist.vue'
+import TodoButton from './components/todoButton.vue'
 
 const todos = ref([
   { id: 1, text: '学习', completed: false },
   { id: 2, text: '完成作业', completed: true },
   { id: 3, text: '锻炼身体', completed: false },
 ])
+
+const filterType = ref('all')
+const filteredTodos = computed(() => {
+  if (filterType.value === 'all') {
+    return todos.value
+  } else if (filterType.value === 'completed') {
+    return todos.value.filter((item) => item.completed)
+  } else if (filterType.value === 'uncompleted') {
+    return todos.value.filter((item) => !item.completed)
+  }
+})
 </script>
 
 <template>
   <div id="todo-list-app">
-    <todolist :todolist="todos"> </todolist>
+    <todolist :todolist="filteredTodos"> </todolist>
+    <todoButton @filter-type="filterType = $event"></todoButton>
   </div>
 </template>
 
---
+++ src/components/todoButton.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <button
+      type="button"
+      class="btn"
+      :class="{
+        'btn-primary': selectedType === 'all',
+        'btn-secondary': selectedType !== 'all',
+      }"
+      @click="emitFilter('all')"
+    >
+      全部
+    </button>
+    <button
+      type="button"
+      class="btn"
+      :class="{
+        'btn-primary': selectedType === 'completed',
+        'btn-secondary': selectedType !== 'completed',
+      }"
+      @click="emitFilter('completed')"
+    >
+      已完成
+    </button>
+    <button
+      type="button"
+      class="btn"
+      :class="{
+        'btn-primary': selectedType === 'uncompleted',
+        'btn-secondary': selectedType !== 'uncompleted',
+      }"
+      @click="emitFilter('uncompleted')"
+    >
+      未完成
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue'
+
+const emit = defineEmits(['filter-type'])
+
+const selectedType = ref('all')
+
+const emitFilter = (type) => {
+  selectedType.value = type
+  emit('filter-type', type)
+}
+</script>
+
+<style scoped>
+.btn-group {
+  width: 500px;
+  margin: 30px auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.btn-primary {
+  background-color: #007bff;
+}
+.btn-secondary {
+  background-color: #6c757d;
+}
+</style>
